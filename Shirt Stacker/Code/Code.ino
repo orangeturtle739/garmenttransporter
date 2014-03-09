6ca4f3485b9dc396bc3c072c1807a291b36bd350
Jacob <swimgiraffe435@gmail.com>
2014-03-09 08:32:56 -0400

    Add lots of documentation and update code.

--- Shirt Stacker/Code/Code.ino
+++ Shirt Stacker/Code/Code.ino
@@ -3,48 +3,64 @@
 void setup(){
   Robot.begin(); // initialize the library
   Robot.beginSpeaker();
+  Robot.beginTFT();
 }
 
 void loop(){
 
+  //Print
+  Robot.debugPrint(111);
+
+  //Get inpout
   int button = BUTTON_NONE;
   while(button==BUTTON_NONE) {
     button = Robot.keyboardRead();
   }
   Robot.beep(BEEP_SIMPLE);
 
-  delay(15000);
+  //Raise the shirt
+  delay(14000);
   Robot.motorsWrite(0,255);
-  delay(10000);
+  delay(12000);
   Robot.motorsWrite(0,0);
 
+  int pos;
   if( button == BUTTON_DOWN){
-    Robot.motorsWrite(255,0);
-    delay(1000);
-    Robot.motorsWrite(0,0);
+    pos = 3;
   } 
   else if( button == BUTTON_RIGHT){
-    Robot.motorsWrite(255,0);
-    delay(2000);
-    Robot.motorsWrite(0,0);
+    pos = 2;
   } 
-  else if( button == BUTTON_UP){
-    Robot.motorsWrite(255,0);
-    delay(3000);
-    Robot.motorsWrite(0,0);
+  else{
+    pos = 1;
   }
-  
+
+  //Print out the positon
+  Robot.debugPrint(pos);
+
+  //Go there
+  rotateToPos(pos, -255);
+
+  //Dump the shirt
+  Robot.motorsWrite(0,255);
+  delay(4000);
+
+  //Go back
+  rotateToPos(pos,255);
+
+  //Lower the tray
+  Robot.motorsWrite(0,-255);
+  delay(7500);
+
+  //Stop everything
   Robot.motorsWrite(0,0);
 }
 
-
-
-
-
-
-
-
-
+void rotateToPos(double spot, int power) {
+  Robot.motorsWrite(power,0);
+  delay(spot*1000);
+  Robot.motorsWrite(0,0);
+}
 
 
 
